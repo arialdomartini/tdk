5f17890b1929bb6770b3450460f3b874dea3b9fa
Arialdo Martini <arialdomartini@gmail.com>
2022-09-21 18:19:23 +0200

    Single transaction is no special case

--- src/NeatCoinKata/Dojo/DataTypes/Ledger.cs
+++ src/NeatCoinKata/Dojo/DataTypes/Ledger.cs
@@ -1,18 +1,12 @@
 using System.Collections.Immutable;
-using System.Linq;
 
 namespace NeatCoinKata.Dojo.DataTypes;
 
 internal record Ledger(ImmutableList<Transaction> Transactions)
 {
-    internal Transaction? Transaction => !Transactions.Any()? null : Transactions.Single();
-    
     internal static Ledger Empty => 
-        new(ImmutableList<Transaction>.Empty);
+        WithTransactions(ImmutableList<Transaction>.Empty);
 
-    internal static Ledger WithTransaction(Transaction transaction) => 
-        WithTransactions(ImmutableList.Create(transaction));
-    
     internal static Ledger WithTransactions(ImmutableList<Transaction> transactions) => 
         new(transactions);
 }
